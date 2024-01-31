5027c31d2493e0b05210addbfb619366510a818e
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-01-30 20:40:05 -0600

    Update leerImagen.py

--- leerImagen.py
+++ leerImagen.py
@@ -10,7 +10,22 @@ print("Valor de un punto y un canal especifico: " + str(singleValue))
 print("Valores de un pixel: " + str(pixelValues))
 
 cropped_image = colorImage[119:219, 229:329, :]
+blue = colorImage.copy()
+blue[:, :, 1] = blue[:, :, 1] * 0
+blue[:, :, 2] = blue[:, :, 2] * 0
+
+filas, cols, chan = colorImage.shape
+red = np.zeros([filas, cols, chan], dtype=np.uint8)
+red[:, :, 2] = colorImage[:, :, 2]
+
+filas, cols, chan = colorImage.shape
+yellow = np.zeros([filas, cols, chan])
+yellow[:, :, 2] = colorImage[:, :, 2]
+yellow = yellow / 255
 
 cv2.imshow('Imagen', colorImage)
+cv2.imshow('tonos azules', blue)
+cv2.imshow('tonos rojos', red)
+cv2.imshow('tonos amarillos', yellow)
 cv2.imshow('Recorte', cropped_image)
 cv2.waitKey(0)
