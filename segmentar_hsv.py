18a5d0ac2f26de603fef1ee0a83db2705b65d45e
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-02-07 15:41:09 -0600

    segmentacion HSV

--- segmentar_hsv.py
+++ segmentar_hsv.py
@@ -4,28 +4,28 @@ from matplotlib import pyplot as plt
 import time
 
 Im1 = cv2.imread('data/color.jpg')
-Im1lab = cv2.cvtColor(Im1, cv2.COLOR_BGR2HSV)
-H = Im1lab[:, :, 0]
-S = Im1lab[:, :, 1]  # Tomamos canal S (verdes negativo (0-127) y rojos positivo (128-25))
-V = Im1lab[:, :, 2]  # tomamos canal b (positivos amarillo, negativos azules)
+Im1hsv = cv2.cvtColor(Im1, cv2.COLOR_BGR2HSV)
+H = Im1hsv[:, :, 0]
+S = Im1hsv[:, :, 1]  # Tomamos canal S (verdes negativo (0-127) y rojos positivo (128-25))
+V = Im1hsv[:, :, 2]  # tomamos canal b (positivos amarillo, negativos azules)
 H = np.double(H) # Conversión tipo de dato
 S = np.double(S)
 V = np.double(V)
 
 
 K = 0  # Ajuste para determinar tonos de rojo detectados
-z = (np.bitwise_and(V < 0, np.abs(V) > np.abs(S)))
+z = (np.bitwise_and(H >= 11, H <= 35))
 
+new_H = H
 new_s = S
 new_v = V
-new_s[z == 1] = np.abs(V[z == 1])
-new_v[z == 1] = S[z == 1]
-new_s[z == 1] = new_s[z == 1]
-Im1lab[:, :, 1] = new_s - 128
-Im1lab[:, :, 2] = new_v - 128
+new_H[z == 1] = new_H[z == 1] + 100
+Im1hsv[:, :, 0] = new_H
+Im1hsv[:, :, 1] = new_s
+Im1hsv[:, :, 2] = new_v
 
-Im1lab = np.uint8(Im1lab)
-Im2 = cv2.cvtColor(Im1lab, cv2.COLOR_LAB2BGR)
+Im1hsv = np.uint8(Im1hsv)
+Im2 = cv2.cvtColor(Im1hsv, cv2.COLOR_HSV2BGR)
 Im1 = cv2.cvtColor(Im1, cv2.COLOR_BGR2RGB)
 Im2 = cv2.cvtColor(Im2, cv2.COLOR_BGR2RGB)
 plt.subplot(1, 3, 1), plt.imshow(Im1, ), plt.title('original')
